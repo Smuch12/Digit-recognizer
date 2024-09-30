7b61838f8e0bc307c4a5f204a2bbb86a607b1bc7
Jonathan.Ski <vup019@uib.no>
2024-09-30 13:33:26 +0200

    Started code added

---
+++ digit_recognizer.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Help: https://github.com/bjerkvik/DigitRecognizer-INF264/blob/main/INF264_Project2_DigitRecognizer.ipynb
+
+from sklearn.model_selection import train_test_split, KFold, cross_validate, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import f1_score, balanced_accuracy_score, confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.dummy import DummyClassifier
+
+data = np.load('dataset.npz')
+X = data['X']
+y = data['y']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
