8e54e8dab16b8ce7d3a78039a447c0dcbd4527ac
Smuch12 <jonathanks222@gmail.com>
2024-10-02 13:59:32 +0200

    Problem 1, good start

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
+# Use this file for testing.
 # Help: https://github.com/bjerkvik/DigitRecognizer-INF264/blob/main/INF264_Project2_DigitRecognizer.ipynb
 
 from sklearn.model_selection import train_test_split, KFold, cross_validate, GridSearchCV
@@ -16,3 +18,6 @@ X = data['X']
 y = data['y']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+corrupted_data = np.load('corrupt_dataset.npz')
+X_corrupted = corrupted_data['X']
